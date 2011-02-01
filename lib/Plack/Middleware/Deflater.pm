4b23f226503c0b6c881af44fdb77da0fd573e7b6
Masahiro Nagano <m-nagano@livedoor.jp>
2011-02-01 17:42:40 +0900

    fixed synopsis

--- lib/Plack/Middleware/Deflater.pm
+++ lib/Plack/Middleware/Deflater.pm
@@ -128,12 +128,13 @@ Plack::Middleware::Deflater - Compress response body with Gzip or Deflate
       my $app = shift;
       sub {
           my $env = shift;
+          my $ua = $env->{HTTP_USER_AGENT} || '';
           # Netscape has some problem
-          $env->{"psgix.gzip-only-text/html"} = 1 if $env->{HTTP_USER_AGENT} =~ m!^Mozilla/4!;
+          $env->{"psgix.gzip-only-text/html"} = 1 if $ua =~ m!^Mozilla/4!;
           # Netscape 4.06-4.08 have some more problems
-          $env->{"psgix.no-gzip"} = 1 if $env->{HTTP_USER_AGENT} =~ m!^Mozilla/4\.0[678]!;
+          $env->{"psgix.no-gzip"} = 1 if $ua =~ m!^Mozilla/4\.0[678]!;
           # MSIE (7|8) masquerades as Netscape, but it is fine
-          if ( $env->{HTTP_USER_AGENT} =~ m!\bMISE (?:7|8)! ) {
+          if ( $ua =~ m!\bMSIE (?:7|8)! ) {
               $env->{"psgix.no-gzip"} = 0;
               $env->{"psgix.gzip-only-text/html"} = 0;
           }
