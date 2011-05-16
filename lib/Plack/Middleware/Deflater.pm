ab9611204f6d9782257391145cef7f00dbf135ba
Masahiro Nagano <kazeburo@gmail.com>
2011-05-16 13:00:27 +0900

    fixed synopsis for passing Test::Synopsis

--- lib/Plack/Middleware/Deflater.pm
+++ lib/Plack/Middleware/Deflater.pm
@@ -122,27 +122,31 @@ Plack::Middleware::Deflater - Compress response body with Gzip or Deflate
 
 =head1 SYNOPSIS
 
-  enable sub {
-      my $app = shift;
-      sub {
-          my $env = shift;
-          my $ua = $env->{HTTP_USER_AGENT} || '';
-          # Netscape has some problem
-          $env->{"psgix.compress-only-text/html"} = 1 if $ua =~ m!^Mozilla/4!;
-          # Netscape 4.06-4.08 have some more problems
-          $env->{"psgix.no-compress"} = 1 if $ua =~ m!^Mozilla/4\.0[678]!;
-          # MSIE (7|8) masquerades as Netscape, but it is fine
-          if ( $ua =~ m!\bMSIE (?:7|8)! ) {
-              $env->{"psgix.no-compress"} = 0;
-              $env->{"psgix.compress-only-text/html"} = 0;
-          }
-          $app->($env);
-      }
-  };
-  enable "Deflater",
-      content_type => ['text/css','text/html','text/javascript','application/javascript'],
-      vary_user_agent => 1;
+  use Plack::Builder;
 
+  builder {
+    enable sub {
+        my $app = shift;
+        sub {
+            my $env = shift;
+            my $ua = $env->{HTTP_USER_AGENT} || '';
+            # Netscape has some problem
+            $env->{"psgix.compress-only-text/html"} = 1 if $ua =~ m!^Mozilla/4!;
+            # Netscape 4.06-4.08 have some more problems
+             $env->{"psgix.no-compress"} = 1 if $ua =~ m!^Mozilla/4\.0[678]!;
+            # MSIE (7|8) masquerades as Netscape, but it is fine
+            if ( $ua =~ m!\bMSIE (?:7|8)! ) {
+                $env->{"psgix.no-compress"} = 0;
+                $env->{"psgix.compress-only-text/html"} = 0;
+            }
+            $app->($env);
+        }
+    };
+    enable "Deflater",
+        content_type => ['text/css','text/html','text/javascript','application/javascript'],
+        vary_user_agent => 1;
+    sub { [200,['Content-Type','text/html'],["OK"]] }
+  };
 =head1 DESCRIPTION
 
 Plack::Middleware::Deflater is a middleware to encode your response
